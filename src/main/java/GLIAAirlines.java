90c51975787b150f97d6c1cabce806d7d5b51419
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-10-20 15:34:16 +0200

    On nettoie

--- src/main/java/GLIAAirlines.java
+++ src/main/java/GLIAAirlines.java
@@ -36,9 +36,7 @@ public class GLIAAirlines {
                 for (IntVar divider : dividers) System.out.print(divider.getValue() + " ");
             }
         }
-
-        // Affichage d'une unique solution
-
+        
         model.getSolver().printStatistics();
     }
 
